fd11ef463ff7db7a01080c95797be9e2ec41b3c1
santhosh0401 <127771610+santhosh0401@users.noreply.github.com>
2023-03-13 21:31:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('registration-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const dobInput = document.getElementById('dob');
+const termsInput = document.getElementById('terms');
+const userTable = document.getElementById('userTable').getElementsByTagName('tbody')[0];
+
+// Load saved data from web storage, if available
+let savedData = JSON.parse(localStorage.getItem('registrationData'));
+if (savedData) {
+  savedData.forEach((data) => {
+    const newRow = userTable.insertRow();
+    const nameCell = newRow.insertCell(0);
+    const emailCell = newRow.insertCell(1);
+    const passwordCell = newRow.insertCell(2);
+    const dobCell = newRow.insertCell(3);
+    const termsCell = newRow.insertCell(4);
+    nameCell.innerHTML = data.name;
+    emailCell.innerHTML = data.email;
+    passwordCell.innerHTML = data.password;
+    dobCell.innerHTML = data.dob;
+    termsCell.innerHTML = data.termsAccepted ? 'Yes' : 'No';
+  });
+} else {
+  savedData = [];
+}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Additional validation for date of birth
+  const dob = new Date(dobInput.value);
+  const age = (new Date() - dob) / (365 * 24 * 60 * 60 * 1000);
+  if (age < 18 || age > 55) {
+    alert('Please enter a valid date of birth between ages 18 and 55.');
+    return;
+  }
+
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+    dob: dobInput.value,
+    termsAccepted: termsInput.checked,
+  };
+
+  // Save form data to web storage
+  savedData.push(formData);
+  localStorage.setItem('registrationData', JSON.stringify(savedData));
+
+  // Append form data to user table
+  const newRow = userTable.insertRow();
+  const nameCell = newRow.insertCell(0);
+  const emailCell = newRow.insertCell(1);
+  const passwordCell = newRow.insertCell(2);
+  const dobCell = newRow.insertCell(3);
+  const termsCell = newRow.insertCell(4);
+  nameCell.innerHTML = formData.name;
+  emailCell.innerHTML = formData.email;
+  passwordCell.innerHTML = formData.password;
+  const dobDate = new Date(formData.dob);
+  dobCell.innerHTML = dobDate.getFullYear() + '-' + ('0' + (dobDate.getMonth() + 1)).slice(-2) + '-' + ('0' + dobDate.getDate()).slice(-2);
+  termsCell.innerHTML = formData.termsAccepted ? 'Yes' : 'No';
+
+  // Reset form
+  form.reset();
+});
